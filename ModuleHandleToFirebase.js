8e8935e541b11924a5e3763b5d0b78e8ca69e1b8
AnhTuan <voanhtuan13321@gmail.com>
2022-12-09 16:57:07 +0700

    v10.1

---
+++ ModuleHandleToFirebase.js
@@ -0,0 +1,64 @@
+'use strict'
+import { getDatabase, set, get, update, remove, ref, child } from 'firebase/database';
+import { initializeApp } from 'firebase/app';
+
+// config to connect to firebase
+const firebaseConfig = {
+    apiKey: 'AIzaSyAPjGTsG9PuaAQK1RivTCFpbOsYYMkrjws',
+    authDomain: 'smartappdb-25c3d.firebaseapp.com',
+    databaseURL: 'https://smartappdb-25c3d-default-rtdb.firebaseio.com',
+    projectId: 'smartappdb-25c3d',
+    storageBucket: 'smartappdb-25c3d.appspot.com',
+    messagingSenderId: '173484980207',
+    appId: '1:173484980207:web:723c18247e1a892ecda81a',
+    measurementId: 'G-BF7016Z27L',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
+
+// insert data to firebase
+export function insertData(path, data) {
+    set(ref(db, path), data).then(() => {}).catch((error) => console.error('faild:', error));
+};
+
+// get data from firebase
+export function selectData(path, gio, callBack) {
+    const dbref = ref(db);
+    get(child(dbref, path))
+        .then(snapshot => {
+            if (snapshot.exists()) {
+                // truong hop ton tai du lieu theo path, thi goi callBack truyen du lieu vao xu ly tiep
+                callBack(snapshot.val());
+            } else {
+                insertDefaultData(path + '/' + gio);
+            }
+        })
+        .catch(error => {
+            // truong hop loi, khong khong ton tai path trong firebase, thi tao path moi
+            console.error('faild - when select data from firebase: ', error);
+            insertDefaultData(path + '/' + gio);
+        });
+};
+
+function insertDefaultData(path) {
+    const data = { [phut]: 0 };
+    insertData(path, data);
+}
+
+// delete data to firebase
+// const deleteData = function (data) {
+//     remove(ref(db, 'people/tuan'))
+//         .then(() => console.log('sussefully'))
+//         .catch((error) => console.error('faild:', error));
+// };
+
+// update data to firebase
+// const updateData = function () {
+//     update(ref(db, 'people/tuan'), {
+//         name: data,
+//     })
+//         .then(() => console.log('sussefully'))
+//         .catch((error) => console.error('faild:', error));
+// };
---
+++ app.js
@@ -0,0 +1,83 @@
+'use strict';
+import { insertData, selectData } from './ModuleHandleToFirebase.js';
+
+// create url to connect to Firebase
+const paths = {
+    temp: 'temp',
+    hum: 'hum',
+    ssCO2: 'ssCO2',
+    dustDensity: 'dustDensity',
+    votage: 'Votage',
+    current: 'Current',
+    power: 'Power',
+    water: 'Water',
+};
+
+/**
+ * Loop
+ * moi 0.5s lap 1 lan
+ */
+console.log('******************************');
+console.log('*    This app is running     *');
+console.log('*    To stop, use Ctrl + C   *');
+console.log('******************************');
+
+let phutQK = new Date().getMinutes();
+
+setInterval(() => {
+    const today = new Date();
+    const gioHienTai = today.getHours();
+    const phutHienTai = today.getMinutes();
+    const isPhutMoi = phutQK === phutHienTai;
+    
+    if (!isPhutMoi) {
+        phutQK = phutHienTai;
+        console.warn('\n=============== Reading firebase ... ===============');
+    }
+
+    selectAndInsertToFirebase(paths.temp, gioHienTai, phutHienTai, !isPhutMoi);
+    selectAndInsertToFirebase(paths.hum, gioHienTai, phutHienTai, !isPhutMoi);
+    selectAndInsertToFirebase(paths.ssCO2, gioHienTai, phutHienTai, !isPhutMoi);
+    selectAndInsertToFirebase(paths.dustDensity, gioHienTai, phutHienTai, !isPhutMoi);
+    selectAndInsertToFirebase(paths.votage, gioHienTai, phutHienTai, !isPhutMoi);
+    selectAndInsertToFirebase(paths.current, gioHienTai, phutHienTai, !isPhutMoi);
+    selectAndInsertToFirebase(paths.power, gioHienTai, phutHienTai, !isPhutMoi);
+    selectAndInsertToFirebase(paths.water, gioHienTai, phutHienTai, !isPhutMoi);
+}, 500);
+
+function selectAndInsertToFirebase(paths, gioHienTai, phutHienTai, isPhutMoi) {
+    // du lieu nhan lai la mot object
+    selectData(paths, gioHienTai, (value) => {
+        // doi khi nap co du lieu ben cam bien thi comment lai
+        value.now = Math.random() * 20 + 20;
+
+        // truong hop tao gio moi tren firebase
+        (!value[gioHienTai]) && Object.assign(value, { [gioHienTai]: {} });
+        
+        if (paths === 'temp') {
+            let now = value.now;
+            (now > 1000) && (now = now / 100);
+            (now > 100) && (now = now / 10);
+            
+            try {
+                value.now = now.toFixed(2);
+            } catch (e) {
+                value.now = now;
+            }
+        }
+
+        // kiem tra, neu qua phut moi thi cap nhat lai phut
+        if (isPhutMoi) {
+            if (phutHienTai == 0) {
+                gioHienTai--;
+                phutHienTai = 59;
+            } else {
+                phutHienTai--;
+            }
+            value[gioHienTai][phutHienTai] = value.now;
+            console.warn(paths, `${gioHienTai}:${phutHienTai} => ${value.now}`)
+        }
+
+        insertData(paths, value);
+    });
+};
